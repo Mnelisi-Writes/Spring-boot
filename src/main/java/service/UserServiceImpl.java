65d503af0b1723d4785a5705c72275b415775108
Mnelisi-Writes <mpontshanemnelisi2002@gmail.com>
2025-05-16 09:28:44 +0200

    Implement UserServiceImpl with basic user operations using FakeRepo

--- src/main/java/service/UserServiceImpl.java
+++ src/main/java/service/UserServiceImpl.java
@@ -1,4 +1,34 @@
 package service;
 
-public class UserServiceImpl {
+import repo.FakeRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserServiceImpl implements UserService {
+
+    private final FakeRepo fakeRepo;
+
+    @Autowired
+    public UserServiceImpl(FakeRepo fakeRepo) {
+        this.fakeRepo = fakeRepo;
+    }
+
+    @Override
+    public String addUser(String name, String surname) {
+        long id = (long) (Math.random() * 1000); // Simple ID generation
+        fakeRepo.insertUser(id, name, surname);
+        return name + " added";
+    }
+
+    @Override
+    public String removeUser(long id) {
+        return fakeRepo.deleteUser(id) + " removed";
+    }
+
+    @Override
+    public String getUser(long id) {
+        String user = fakeRepo.findUserById(id);
+        return "Hello " + user;
+    }
 }
