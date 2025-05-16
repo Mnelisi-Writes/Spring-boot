fc54fd3c92b5c93963a9897dae9619c50b4fb74b
Mnelisi-Writes <mpontshanemnelisi2002@gmail.com>
2025-05-16 09:27:36 +0200

    Add UserService interface with user management methods

--- src/main/java/service/UserService.java
+++ src/main/java/service/UserService.java
@@ -1,4 +1,7 @@
 package service;
 
-public class UserService {
+public interface UserService {
+    String addUser(String name, String surname);
+    String removeUser(long id);
+    String getUser(long id);
 }
